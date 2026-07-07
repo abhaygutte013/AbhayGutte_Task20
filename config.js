5b2700b7d1469a4de22fcf7ccd523545884bc63b
Abhay Gutte <abhaygutte292@gmail.com>
2026-07-07 11:10:16 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+// EmailJS Configuration
+
+const EMAIL_PUBLIC_KEY = "B_8FKSP9Vcn9gS-bt";
+
+const SERVICE_ID = "service_mrgn9bs";
+
+const BOOKING_TEMPLATE = "template_agoz8sp";
+
+const NEWSLETTER_TEMPLATE = "template_fafj74a";
